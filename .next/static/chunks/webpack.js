a4b6d7f7c2b5bac1b80d3f7e631dec7ba899f6f6
SeneshFitzroy <10952757@students.plymouth.ac.uk>
2025-04-13 02:53:39 +0530

    Update webpack hot-update files with new hash values and add missing hot-update script

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "2c7b2445de38c948"; }
+/******/ 		__webpack_require__.h = function() { return "f402efeeb0341804"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
